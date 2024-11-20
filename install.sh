1ef61cb6b5da73c863de3d898f48d38dd6e369ed
AliPiccioniQC <125677204+AliPiccioniQC@users.noreply.github.com>
2024-11-20 12:18:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# On a fresh MacBook directory is in the PATH but not created.
+mkdir -p /usr/local/bin
 curl -o /usr/local/bin/pixi https://raw.githubusercontent.com/Ali-Piccioni/pixipin/main/pixipin
 chmod a+x /usr/local/bin/pixi
